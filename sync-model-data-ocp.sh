ccc678155458b901722912dd8bb728d3575b2cfc
Juho Inkinen <juho.inkinen@helsinki.fi>
2023-05-08 17:26:20 +0300

    Update models-sync script (from FintoAI-data-YSO)

--- sync-model-data-ocp.sh
+++ sync-model-data-ocp.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
-# Transfers data and projects.d directories to an OpenShift volume that is attached
-# to a pod which is running Annif. The instance {api-annif-org,ai-finto-fi, etc.}
-# which to transfer is given as the first argument.
-# You need to be logged in to the cluster with the oc cli tool.
+# Runs rsync to transfer model data from the current directory to an OpenShift volume
+# that is attached to a pod which is running Annif. The instance
+# {api-annif-org,ai-finto-fi, etc.} to transfer to is given as the argument.
+# You need to be logged to the cluster with the oc tool.
 
 set -e
 
 if [ $# -ne 1 ]
   then
-    echo "Not enough arguments"
+    echo "Not enough arguments; argument 1: destination_instance"
     exit 1
 fi
 
-# echo Syncing to instance $1
 pod=$(oc get pods -l app.kubernetes.io/instance=$1,app.kubernetes.io/name=annif -o name)
-pod=${pod#pod/}
 
+if [[ $pod = *[[:space:]]* ]]
+  then
+    echo "Multiple pod exists; using first"
+    pod=(${pod//$'\n'/ })
+fi
+echo "Target is "$pod
+pod=${pod#pod/}
 if [ -z "${pod}" ]
   then
     echo "No target pod found"
     exit 1
 fi
-# echo Target pod is $pod
 
-rsync --rsh='oc rsh' -avrL --exclude="*train*" --exclude="*zip" data projects.d $pod:/annif-projects/
+rsync --rsh='oc rsh' -avrL --exclude="*train*" --exclude="*zip" --inplace projects.d $pod:/annif-projects
+rsync --rsh='oc rsh' -avrL --exclude="*train*" --exclude="*zip" --inplace data/{projects,vocabs} $pod:/annif-projects/data
